29cadd8d47f57393c4bc1a00615cec50c65db8ed
Puneet <puneetkpoonia672003@gmail.com>
2025-04-14 23:26:38 +0530

    hero page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,32 @@
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@heroui/button';
+import { FaArrowCircleRight } from 'react-icons/fa';
+
 import DefaultLayout from '@/layouts/default';
 
 export default function IndexPage() {
+	const navigate = useNavigate();
+
 	return (
 		<DefaultLayout>
-			<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-				<h1 className="text-3xl">Hello</h1>
-				<h3 className="text-xl">World</h3>
-			</section>
+			<div className="mb-8 flex flex-col justify-center items-center">
+				<h1 className="text-5xl font-bold tracking-tight">
+					Lynx<span className="text-secondary">.</span>
+				</h1>
+				<p className="mt-3 text-lg opacity-70">
+					Simple URL shortening that just works.
+				</p>
+				<div className="mt-6">
+					<Button
+						color="secondary"
+						size="lg"
+						className="text-md"
+						onPress={() => navigate('/sign-in')}
+					>
+						Get started <FaArrowCircleRight />
+					</Button>
+				</div>
+			</div>
 		</DefaultLayout>
 	);
 }
